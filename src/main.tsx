07407b4a4eadb70d12f5783a3034e6b6405bdf5e
Alan Dias <adias7882@gmail.com>
2023-10-15 19:02:44 -0300

    Retira componente desnecessário para corrigir erro no deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import React from 'react'
-import Loader from './components/Loader/loader.tsx'
 
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <React.Suspense fallback={<Loader />}>
         <App />
-    </React.Suspense>
   </BrowserRouter>
 )
