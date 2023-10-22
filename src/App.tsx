1d66a1562f64ab26058aa40ab52112e18804c80c
Alan Dias <adias7882@gmail.com>
2023-10-22 20:52:55 -0300

    Corrige problema para chamar componente de Skeleton após remoção da lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import About from './pages/About';
 import WorkWithMe from './pages/WorkWithMe';
 import Agenda from './pages/Agenda';
-import { SkeletonTheme } from 'react-loading-skeleton';
 
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className = "app">
-      <SkeletonTheme baseColor="#202020" highlightColor="#444">
         < Routes>
           <Route path = "/" element = {<About />}/>
           <Route path = "/construacomigo" element = {<WorkWithMe />}/>
           <Route path = "/agenda" element = {<Agenda />}/>
         </Routes>
-      </SkeletonTheme>
     </div>
   );
 }
