61d2f8546d5f445b0e31c3a4a5456e221f1e528f
Alan Dias <adias7882@gmail.com>
2023-11-03 10:21:45 -0300

    Remove componente desnecessário do react-icons

--- src/pages/ArticlePage/index.tsx
+++ src/pages/ArticlePage/index.tsx
@@ -2,7 +2,6 @@ import { MDXProvider } from '@mdx-js/react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import React from 'react';
-import { BsPersonCircle } from 'react-icons/bs';
 import { AiFillMediumSquare } from 'react-icons/ai';
 import articlesData from '../../data/articles.json';
 import { BiArrowBack } from 'react-icons/bi';
